68f09954d5d145e20f8bc1f92d095579f46b8bc0
faruq <yeemirahman@gmail.com>
2024-05-01 08:17:43 +0100

    completed

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function rangeOfNumbers(lower, upper) {
+  const result = []
+  for (let i = lower; i <= upper; i++) {
+    result.push(i)
+  }
+  return result
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,9 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function addExclamation(str, num) {
+  return str.toUpperCase() + '!'.repeat(num)
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +43,19 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function addMinutes(time, minutesToAdd) {
+  const [hours, minutes] = time.split(':').map(Number)
+  const totalMinutes = hours * 60 + minutes + minutesToAdd
+  const newHours = Math.floor(totalMinutes / 60) % 24
+  const newMinutes = totalMinutes % 60
+  return `${newHours.toString().padStart(2, '0')}:${newMinutes
+    .toString()
+    .padStart(2, '0')}`
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: rangeOfNumbers,
+  b: addExclamation,
+  c: addMinutes
 }
